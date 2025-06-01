697912e922c0a13fbd709d06a4864add2043e807
Svitlana Midianko <s.midianko@gmail.com>
2025-06-01 11:08:28 -0700

    added a local serving.

---
+++ serve.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import webbrowser
+import os
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server at http://localhost:{port}")
+    webbrowser.open(f'http://localhost:{port}')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run() 
